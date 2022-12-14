60571f821b3a374546bbbf968754a7853a589821
PatriciaBernardes <barcelosbernardespatricia@gmail.com>
2022-12-14 18:21:26 -0300

    Upload

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 15px;
+    border: 2px solid #394764;
+    border-radius: 10px;
+    background-color: #212529;
+    color: #FFF;
+    font-size: 24px;
+    font-weight: 500;
+    flex: 1;
+    font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 100px;
+    background-color: #394764;
+    
+    
+    
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+   
+    input{
+        width: 92%;
+        height: 75px;
+        background-color: #212529;
+        border: 1px solid #212529;
+        border-radius: 10px;
+        text-align: right;
+        font-size: 24px;
+        font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+        padding: 0 10px;
+        color: #fff;
+    }
+`
+
